b1aa25be5680a66ba377a31af89ae125a97c151c
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2020-12-18 12:02:55 -0600

    created basic password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,26 @@
 // Assignment code here
 
+// Click the button to generate a password
+// Present series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+function generate(){
+  let pass = "";
+  let str = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$`;
+
+  for(i = 1; i <= 8; i++){
+    let char = Math.floor(Math.random() * str.length +1);
+
+    pass += str.charAt(char);
+  }
+return pass;
+}
+// WHEN prompted for character types to include in the password: choose lowercase, uppercase, numeric, and/or special characters
+// Input should be validated and at least one character type should be selected
+// THEN a password is generated that matches the selected criteria after prompts are answered
+// Generate password display in an alert or written to the page
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
